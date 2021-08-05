5ceb18e2b798dc893cf874797ee2b2154b156c09
dennisMSF <87782818+dennisMSF@users.noreply.github.com>
2021-08-05 18:37:48 -0500

    Create main.cpp

---
+++ Class_license/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class moto:public car{
+public:
+    int tipo;
+};
+class carro:public car{
+    int marca;
+};
+class truck:public carro{
+    int lel;
+};
+int main(){
+    
+    return 0;
+    
+}
